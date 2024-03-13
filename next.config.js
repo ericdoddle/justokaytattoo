5fcc60b7875f7b74691f4e49653af76b397f3bf0
Camryn Perry <camrynpaigeperry@gmail.com>
2024-03-13 19:34:08 -0400

    clean up unnecessary deploy steps; remove commented out code

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   output: 'export',
   basePath: '/justokaytattoo-camryn',
   images: { unoptimized: true }
 };
 
-// const withMDX = nextMDX({
-//   extension: /\.mdx?$/,
-//   options: {
-//     remarkPlugins: [remarkGfm],
-//     rehypePlugins: [rehypePrism],
-//   },
-// });
 module.exports = nextConfig;
